f4f9c281be64e8f2b665c34bf37fe8a0d2d455e2
bongomin <bongomin50@gmail.com>
2023-02-16 13:33:05 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - program what uses printf function
+*Return: 0(successful)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
