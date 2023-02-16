5a56d1626a64ecaa2e09ce9358fdbcdc05428bbe
bongomin <bongomin50@gmail.com>
2023-02-16 13:22:33 -0800

    4-pusts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - contains a funtion puts that prints a given text
+*Return: 0 (implies success in running the program)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle,\n");
+return (0);
+}
