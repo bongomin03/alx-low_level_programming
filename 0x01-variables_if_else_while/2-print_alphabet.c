ebca84bf6f8e3aee55fc2d3ebadcecf30155fd8b
bongomin <bongomin50@gmail.com>
2023-02-17 12:55:53 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - pinrt small letters
+* Return: 0(success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
