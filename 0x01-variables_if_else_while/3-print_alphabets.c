75a51c0897a239d903342de692b7e9b1cfe3e3f1
bongomin <bongomin50@gmail.com>
2023-02-17 13:12:53 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - pinrt capital letters
+* Return: 0(success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
